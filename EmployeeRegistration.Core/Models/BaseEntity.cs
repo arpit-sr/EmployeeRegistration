80ecd193c19f4f465474899515b3713cc380e287
arpit-sr <53820238+arpit-sr@users.noreply.github.com>
2021-03-15 23:15:57 +0530

    Nuget updated and Model setup

---
+++ EmployeeRegistration.Core/Models/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeRegistration.Core.Models
+{
+    public abstract class BaseEntity
+    {
+        public string pKey { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+
+        public BaseEntity()
+        {
+            this.pKey = Guid.NewGuid().ToString();
+            this.CreatedAt = DateTime.Now;
+        }
+    }
+}
---
+++ EmployeeRegistration.Core/Models/EmployeeDetail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeRegistration.Core.Models
+{
+    public class EmployeeDetail : BaseEntity
+    {
+        [StringLength(15)]
+        public string FirstName { get; set; }
+        [StringLength(15)]
+        public string LastName { get; set; }
+        public string EmailID { get; set; }
+        [Range(0,99999999)]
+        public int EmployeeID { get; set; }
+        public string EmployeeLocation { get; set; }
+        public string Image { get; set; }
+    }
+}
