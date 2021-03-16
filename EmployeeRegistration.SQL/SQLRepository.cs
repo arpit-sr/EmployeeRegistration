20b5f3d51a6154eb4b5aa502dd15e15d0af0dcb7
arpit-sr <53820238+arpit-sr@users.noreply.github.com>
2021-03-16 13:33:09 +0530

    Added generics class

---
+++ EmployeeRegistration.SQL/SQLRepository.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeRegistration.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeRegistration.SQL
+{
+    public class SQLRepository<T> where T : BaseEntity
+    {
+
+    }
+}
---
+++ EmployeeRegistration.WebUI/Controllers/EmployeeManagerController.cs
@@ -0,0 +1,25 @@
+﻿using EmployeeRegistration.Core.Models;
+using EmployeeRegistration.SQL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeRegistration.WebUI.Controllers
+{
+    public class EmployeeManagerController : Controller
+    {
+        SQLRepository<EmployeeDetail> context;
+
+        public EmployeeManagerController()
+        {
+            context = new SQLRepository<EmployeeDetail>();
+        }
+        // GET: EmployeeManager
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
