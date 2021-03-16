8de06aa4521a812807f4847575f2b6351c075ca9
arpit-sr <53820238+arpit-sr@users.noreply.github.com>
2021-03-17 01:16:16 +0530

    implemented sql repository

--- EmployeeRegistration.Core/Contracts/ISQLRepository.cs
+++ EmployeeRegistration.Core/Contracts/ISQLRepository.cs
@@ -5,10 +5,10 @@ namespace EmployeeRegistration.Core.Contracts
 {
     public interface ISQLRepository<T> where T : BaseEntity
     {
-       // IQueryable<T> Collection();
+        IQueryable<T> Collection();
         void Commit();
         void Delete(string Id);
-     //   T Find(string Id);
+        T Find(string Id);
         void Insert(T t);
         void Update(T t);
     }
--- EmployeeRegistration.SQL/SQLRepository.cs
+++ EmployeeRegistration.SQL/SQLRepository.cs
@@ -2,6 +2,7 @@
 using EmployeeRegistration.Core.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,39 +11,47 @@ namespace EmployeeRegistration.SQL
 {
     public class SQLRepository<T> : ISQLRepository<T> where T : BaseEntity
     {
-
-        public SQLRepository()
+        internal DataContext context;
+        internal DbSet<T> dbSet;
+        public SQLRepository(DataContext context)
         {
-            
+            this.context = context;
+            this.dbSet = context.Set<T>();
         }
-        //public IQueryable<T> Collection()
-        //{
-            
-        //}
+
+        public IQueryable<T> Collection()
+        {
+            return dbSet;
+        }
 
         public void Commit()
         {
-            
+            context.SaveChanges();
         }
 
         public void Delete(string Id)
         {
-           
+            var t = Find(Id);
+            if (context.Entry(t).State == EntityState.Detached)
+                dbSet.Attach(t);
+
+            dbSet.Remove(t);
         }
 
-        //public T Find(string Id)
-        //{
-           
-        //}
+        public T Find(string Id)
+        {
+            return dbSet.Find(Id);
+        }
 
         public void Insert(T t)
         {
-           
+            dbSet.Add(t);
         }
 
         public void Update(T t)
         {
-           
+            dbSet.Attach(t);
+            context.Entry(t).State = EntityState.Modified;
         }
     }
 }
