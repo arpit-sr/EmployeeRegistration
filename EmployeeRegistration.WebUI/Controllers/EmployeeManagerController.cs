43e7d5123068e130cf54d0120e95c8f721723a45
arpit-sr <53820238+arpit-sr@users.noreply.github.com>
2021-03-17 12:08:30 +0530

    UI methods built

--- EmployeeRegistration.WebUI/Controllers/EmployeeManagerController.cs
+++ EmployeeRegistration.WebUI/Controllers/EmployeeManagerController.cs
@@ -20,7 +20,104 @@ namespace EmployeeRegistration.WebUI.Controllers
         // GET: EmployeeManager
         public ActionResult Index()
         {
-            return View();
+            List<EmployeeDetail> products = context.Collection().ToList();
+            return View(products);
+        }
+
+        public ActionResult Create()
+        {
+            EmployeeDetail product = new EmployeeDetail();
+            return View(product);
+        }
+
+        [HttpPost]
+        public ActionResult Create(EmployeeDetail product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+            else
+            {
+                context.Insert(product);
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Edit(String Id)
+        {
+            EmployeeDetail product = context.Find(Id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(EmployeeDetail product, String Id)
+        {
+            EmployeeDetail productToEdit = context.Find(Id);
+
+            if (productToEdit == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(product);
+                }
+
+                productToEdit.FirstName = product.FirstName;
+                productToEdit.LastName = product.LastName;
+                productToEdit.EmailID = product.EmailID;
+                productToEdit.EmployeeID = product.EmployeeID;
+                productToEdit.EmployeeLocation = product.EmployeeLocation;
+                productToEdit.Image = product.Image;
+
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Delete(String Id)
+        {
+            EmployeeDetail productToDelete = context.Find(Id);
+
+            if (productToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(productToDelete);
+            }
+        }
+
+        [HttpPost]
+        [ActionName("DELETE")]
+        public ActionResult ConfirmDelete(String Id)
+        {
+            EmployeeDetail productToDelete = context.Find(Id);
+
+            if (productToDelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                context.Delete(Id);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
         }
     }
 }
